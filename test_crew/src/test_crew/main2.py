02ac1cd92a2e3914e5720ecabd794da964637c0a
Marc Lebreuil <marc.lebreuil@nagra.com>
2024-12-02 14:39:32 +0100

    update test

---
+++ test_crew/src/test_crew/main2.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+import sys
+import warnings
+import yaml
+
+from test_crew.crew import TestCrew
+
+warnings.filterwarnings("ignore", category=SyntaxWarning, module="pysbd")
+
+# This main file is intended to be a way for you to run your
+# crew locally, so refrain from adding unnecessary logic into this file.
+# Replace with inputs you want to test with, it will automatically
+# interpolate any tasks and agents information
+
+
+def run():
+    """
+    Run the crew.
+    """
+    with open('input.yaml', 'r') as file:
+        inputs = yaml.safe_load(file)
+    TestCrew().crew().kickoff(inputs=inputs)
+
+
+def train():
+    """
+    Train the crew for a given number of iterations.
+    """
+    with open('input.yaml', 'r') as file:
+        inputs = yaml.safe_load(file)
+    try:
+        TestCrew().crew().train(n_iterations=int(sys.argv[1]), filename=sys.argv[2], inputs=inputs)
+
+    except Exception as e:
+        raise Exception(f"An error occurred while training the crew: {e}")
+
+def replay():
+    """
+    Replay the crew execution from a specific task.
+    """
+    try:
+        TestCrew().crew().replay(task_id=sys.argv[1])
+
+    except Exception as e:
+        raise Exception(f"An error occurred while replaying the crew: {e}")
+
+def test():
+    """
+    Test the crew execution and returns the results.
+    """
+    inputs = {
+        "topic": "AI LLMs"
+    }
+    try:
+        TestCrew().crew().test(n_iterations=int(sys.argv[1]), openai_model_name=sys.argv[2], inputs=inputs)
+
+    except Exception as e:
+        raise Exception(f"An error occurred while replaying the crew: {e}")
