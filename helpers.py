9292c45e05cd9cc5b9cef8d6a6679c1e675de9e6
David Gallay <david.gallay@nagra.com>
2025-02-25 18:02:56 +0100

    Added helpers to connect with ssh to a remote server serving ollama

--- helpers.py
+++ helpers.py
@@ -2,6 +2,8 @@
 
 import os
 from dotenv import load_dotenv, find_dotenv
+from crewai import Agent, Task, Crew, LLM
+
 
 # these expect to find a .env file at the directory above the lesson.                                                                                                                     # the format for that file is (without the comment)                                                                                                                                       #API_KEYNAME=AStringThatIsTheLongAPIKeyFromSomeService                                                                                                                                     
 def load_env():
@@ -11,3 +13,20 @@ def get_openai_api_key():
     load_env()
     openai_api_key = os.getenv("OPENAI_API_KEY")
     return openai_api_key
+
+
+
+def _get_default_llm_name():
+    return os.getenv("DEFAULT_LLM_MODEL", "ollama/llama3.2")
+
+def _get_llm_base_url():
+    return os.getenv("LLM_BASE_URL") or os.getenv("DEFAULT_LLM_BASE_URL") or None
+
+def get_default_llm():
+    model_name = _get_default_llm_name()
+    base_url = _get_llm_base_url()
+    print(model_name, base_url)
+    return LLM(model=model_name, base_url=base_url)
+
+
+DEFAULT_LLM = get_default_llm()
---
+++ tools.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+
+if [[ -z "$DEFAULT_LLM_MODEL" && -n "$AVAILABLE_LLM_MODELS" && -n "$(which fzf)" ]]; then
+    export DEFAULT_LLM_MODEL=`echo "$AVAILABLE_LLM_MODELS" | fzf`
+fi
+
+export DEFAULT_OLLAMA_HOST="${DEFAULT_OLLAMA_HOST:-localhost}"
+export DEFAULT_OLLAMA_PORT="${DEFAULT_OLLAMA_PORT:-11434}"
+
+export OLLAMA_HOST="${OLLAMA_HOST:-$DEFAULT_OLLAMA_HOST}"
+export OLLAMA_PORT="${OLLAMA_PORT:-$DEFAULT_OLLAMA_PORT}"
+
+export DEFAULT_LLM_BASE_URL="http://$OLLAMA_HOST:$OLLAMA_PORT"
+export LLM_BASE_URL="${LLM_BASE_URL:-$DEFAULT_LLM_BASE_URL}"
+
+
+# https://superuser.com/a/1658519
+forward_ssh() {
+    ssh -NT -o ServerAliveInterval=60 -o ServerAliveCountMax=10 -o ExitOnForwardFailure=yes -L "$OLLAMA_PORT:localhost:$DEFAULT_OLLAMA_PORT" $@
+}
+
+connect_gpu_server() {
+    
+    if [[ -z "$SSH_OLLAMA" ]]; then
+        echo 'Environment variable $SSH_OLLAMA is not defined'
+        exit 1
+    fi
+    forward_ssh "$SSH_OLLAMA"
+}
+
+if [[ -n "$SSH_OLLAMA" ]]; then
+    connect_gpu_server 2>/dev/null &
+fi
